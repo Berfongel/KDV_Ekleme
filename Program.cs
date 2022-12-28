f18ba24f958a5a196165c024f92f0f78ec673784
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-28 16:13:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+double fiyat,KDVHesabı,KDVliTutari;
+Console.WriteLine("Fiyat giriniz : ");
+fiyat = Convert.ToDouble(Console.ReadLine());
+KDVHesabı = ((fiyat*18)/100);
+KDVHesabı = fiyat + KDVHesabı;
+Console.WriteLine("KDVli tutarı : " + KDVHesabı);
+
